2a087844a8c0b37b7737b9ae827c860405dd0d0e
Veit Poigner <v.poigner@gmail.com>
2022-01-31 16:09:26 +0100

    sets up web UI

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -36,31 +36,8 @@ function refactorPlayRound(player1Selection, player2Selection) {
     } else if ((player1Selection == "Scissors") && (player2Selection == "Paper")) {
         return returnResultTextAsString(player1Selection, player2Selection);
     } else {
-        return returnResultTextAsString(player2Selection, player1Selection)+ "! Player 2 Wins!";
+        return returnResultTextAsString(player2Selection, player1Selection) + "! Player 2 Wins!";
     }
-}
-
-function game() {
-    let score = 0;
-    for (i = 0; i < 5; i++) {
-        let player = sanitizePlayerInput(prompt('Rock, Paper, Scissors'));
-        let computer = computerPlay();
-        console.log(refactorPlayRound(player, computer));
-        score = score + evaluateWinner(refactorPlayRound(player, computer));
-    }
-    if (score > 0) {
-        console.log("Player wins!");
-    } else if (score < 0) {
-        console.log("Computer wins!");
-    } else {
-        console.log("It\'s a draw!");
-    }
-}
-
-function evaluateWinner(printingString) {
-    if (printingString.slice(0, 3) == "Dra") {return 0;}
-    else if (printingString.slice(-5) == "Wins!") {return -1;}
-    return 1;
 }
 
 function returnResultTextAsString(playerOneSelection, playerTwoSelection, beatText = " beats ") {
@@ -72,13 +49,12 @@ function sanitizePlayerInput(playerInput) {
 }
 
 
-function testPlayRound (test1, test2) {
+function testPlayRound(test1, test2) {
     console.log(playRound(test1, test2));
     console.log(refactorPlayRound(test1, test2));
     return playRound(test1, test2) == refactorPlayRound(test1, test2);
 
 }
-game();
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -94,4 +70,57 @@ function firstLetterUppercase(toBeUpped) {
 function allButFirstLowerCase(toBeDowned) {
     let substringed = toBeDowned.substr(1, toBeDowned.length - 1);
     return substringed.toLowerCase();
+}
+
+// UI Events
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', countUiRound);
+    button.addEventListener('click', playUiRound);
+});
+
+//const rock = document.querySelector('#rock');
+//rock.addEventListener('click', playUiRound);
+
+let roundNr = 0;
+function countUiRound(event) {
+    roundNr++;
+}
+
+function playUiRound(event) {
+    announceRoundWinner(playRound(event.target.id, computerPlay()));
+}
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+function announceRoundWinner(winner) {
+    let winningPlayer = "";
+    const resultContainer = document.querySelector('#resultContainer');
+    if (score(winner) > 0) {
+        playerOneScore++;
+        winningPlayer = "Human";
+    }
+    if (score(winner) < 0) {
+        playerTwoScore++;
+        winningPlayer = "Computer";
+    }
+    const result = document.createElement('div');
+    result.classList.add('result');
+    result.textContent = winner + " #" + roundNr + " won by " + winningPlayer + " | Player One Score: " + playerOneScore + " Computer Score: " + playerTwoScore;
+
+    if (playerOneScore >= 5) {alert("Human wins 5 Rounds!")}
+    if (playerTwoScore >= 5) {alert("Computer wins 5 Rounds!")}
+
+    resultContainer.appendChild(result);
+}
+
+function score(playedRound) {
+    if (playedRound.slice(0, 4) == "Draw") {
+        return 0;
+    } else if (playedRound.substr(playedRound.length - 7, playedRound.length) == "2 Wins!") {
+        return -1;
+    } else {
+        return 1;
+    }
 }
